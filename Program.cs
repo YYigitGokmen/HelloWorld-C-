26c1f8b2c873ee1d536bff9c14697d9132ddc26b
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-07-16 07:15:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,10 +7,6 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int a = 5; int y=3;
-            a *= 4;
-            Console.WriteLine(a);
-
             Console.WriteLine("Yılmaz Yiğit Gökmen");
             Console.WriteLine("Patika+'a katılma sebebim yazılım sektörüne kapsamlı ve hızlı bir başlangıç yapmak :)");
         }
